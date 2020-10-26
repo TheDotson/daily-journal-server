78ad8c8ca1c796165f81c3d7da5b12772570e0a5
Michael Dotson <thedotson@icloud.com>
2020-10-26 18:15:40 -0500

    updated gitignore

--- dailyjournal.sql
+++ dailyjournal.sql
@@ -22,3 +22,16 @@ INSERT INTO JournalEntries VALUES (null, 'abc', '123', 1598458548239, 2);
 INSERT INTO JournalEntries VALUES (null, 'Delete', 'Now Deleting', 1598458559152, 1);
 INSERT INTO JournalEntries VALUES (null, 'ANGRY', 'fgfgdfg', 1598557358781, 3);
 INSERT INTO JournalEntries VALUES (null, '678', 'Now Deleting', 1598557373697, 4)
+
+        SELECT
+          e.id,
+          e.concept,
+          e.entry,
+          e.date,
+          e.moodId,
+          m.label
+        FROM JournalEntries e
+        JOIN moods m
+            ON e.moodId = m.id
+
+SELECT * from JournalEntries
